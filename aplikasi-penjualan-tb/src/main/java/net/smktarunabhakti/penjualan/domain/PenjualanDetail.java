5948a74c8234f67350bc1f5cbd262948421d0846
armadeas <ketikdeas@yahoo.com>
2014-05-16 16:01:49 +0700

    berhasil berhasil Horeee

--- aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/domain/PenjualanDetail.java
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/domain/PenjualanDetail.java
@@ -39,5 +39,45 @@ public class PenjualanDetail implements Serializable{
     
     @Column(name = "total_harga")
     private BigDecimal totalHarga;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Penjualan getHeader() {
+        return header;
+    }
+
+    public void setHeader(Penjualan header) {
+        this.header = header;
+    }
+
+    public Barang getBarang() {
+        return barang;
+    }
+
+    public void setBarang(Barang barang) {
+        this.barang = barang;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public BigDecimal getTotalHarga() {
+        return totalHarga;
+    }
+
+    public void setTotalHarga(BigDecimal totalHarga) {
+        this.totalHarga = totalHarga;
+    }
     
 }
