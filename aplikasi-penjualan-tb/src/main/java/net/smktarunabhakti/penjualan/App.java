8fac98c69f6f7163eae334e427a8e948bc22c014
armadeas <ketikdeas@yahoo.com>
2014-05-17 14:12:47 +0700

    berhasil berhasil Horeee

--- aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
@@ -1,5 +1,7 @@
 package net.smktarunabhakti.penjualan;
 
+import net.smktarunabhakti.penjualan.domain.Barang;
+import net.smktarunabhakti.penjualan.service.AppService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,10 +12,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App 
 {
     private static  ApplicationContext appCtx;
+    private static AppService appService;
     public static void main( String[] args )
     {
         appCtx = new ClassPathXmlApplicationContext(
         "classpath:net/smktarunabhakti/penjualan/"
         + "applicationContext.xml");
+        appService = (AppService)appCtx.getBean("appService");
+        saveBarang();
+    }
+    public static void saveBarang(){
+        Barang b = new Barang();
+        b.setKodeBarang("Jono");
+        b.setNamaBarang("Juned");
+        
+        appService.simpanBarang(b);
     }
 }
